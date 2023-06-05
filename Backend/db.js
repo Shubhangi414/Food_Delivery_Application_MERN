3c6d7d58cb2a7864b60979caf02a11b36fae8a0a
Shubhangi414 <shubhj1798@gmail.com>
2023-06-05 11:57:32 +0530

    Food Delivery App

---
+++ Backend/db.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://0.0.0.0:27017/Foodies17'
+
+const mongoDB = async () => {
+    
+        mongoose.set('strictQuery', true);
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async(err, result)=>{
+
+        if(err) console.log(err);
+        else{
+            console.log("connected");
+            const fetched_data = mongoose.connection.db.collection("food_items")
+            fetched_data.find({}).toArray(async function (err, data) {
+
+const foodCategory = await mongoose.connection.db.collection("foodCategory")
+            foodCategory.find({}).toArray(function(err,catData){
+                if (err)
+                    console.log(err);
+                else{
+                    global.food_items = data;
+                    global.foodCategory = catData;
+                }    
+                 
+               
+            })
+                // if (err)
+                //     console.log(err);
+                // else
+                //  global.food_items = data;
+                //  console.log(global.food_items);
+    
+            })
+        }
+    })
+        
+
+
+}
+
+module.exports = mongoDB;
